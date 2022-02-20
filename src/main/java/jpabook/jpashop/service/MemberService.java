0832a37a0a525709a2f38ae5d287394a0019a700
audgks5551 <audgks5551@daum.net>
2022-02-20 18:10:34 +0900

    회원 도메인 개발

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,63 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) // 등록 말고 조회할 때는 읽기전용으로 등록해 주면 JPA가 최적화함
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원가입
+     * @param member
+     * @return member.getId();
+     */
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member); // 회원 중복 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /**
+     * 회원 중복 검증
+     * @param member
+     */
+    private void validateDuplicateMember(Member member) {
+        // EXCEPTION
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 회원 전체 조회
+     * @return List<Member>
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 단건 조회
+     * @param memberId
+     * @return Member
+     */
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
+
+
+
+
+}
